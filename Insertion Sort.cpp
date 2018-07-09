d927f42ee25236cbc382d7e36bbee9c87a2e4287
Galang Chairul taqwa <40758224+galangchtaqwa@users.noreply.github.com>
2018-07-09 15:59:44 +0700

    Praktikum 3 (Sorting)

---
+++ Insertion Sort.cpp
@@ -0,0 +1,36 @@
+// Insertion Sort
+#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+using namespace std;
+void insertionSort(int arr[], int n)
+{
+int i, key, j;
+for (i = 1; i < n; i++)
+{
+key = arr[i];
+j = i-1;
+while (j >= 0 && arr[j] > key)
+{
+arr[j+1] = arr[j];
+j = j-1;
+}
+arr[j+1] = key;
+}
+}
+void printArray(int arr[], int n)
+{
+int i;
+for (i=0; i < n; i++)
+cout<<" "<<arr[i];
+cout<<endl;
+}
+int main()
+{
+int arr[] = {12,11,13,5,6,8,4,3,20,21,25};
+int n = sizeof(arr)/sizeof(arr[0]);
+cout<<"Array yang telah terurut :\n";
+insertionSort(arr, n);
+printArray(arr, n);
+return 0;
+}
